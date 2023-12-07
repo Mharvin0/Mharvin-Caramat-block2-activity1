6f03826526edb01dcafcde14842303b0af45b4b2
Mharvin Caramat <caramatmharvin@gmail.com>
2023-12-07 22:05:58 +0800

    2nd Push

--- app/src/main/java/com/caramat/mharvin/block2/p1/quiz/MainActivity.kt
+++ app/src/main/java/com/caramat/mharvin/block2/p1/quiz/MainActivity.kt
@@ -2,6 +2,7 @@ package com.caramat.mharvin.block2.p1.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.Scanner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,3 +10,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+    fun main (args: Array<String>){
+        println("Hello! This is Mharvin Caramat from BSIT2 Block 2")
+
+    }
